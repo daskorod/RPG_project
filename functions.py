ea030712562ea3db767e318fd7856312f0efa21b
daskorod <daskorod@gmail.com>
2017-03-17 12:58:54 +0300

    word wrap

--- functions.py
+++ functions.py
@@ -20,45 +20,78 @@ class Compose_dialog_tree ():
 		self.an = 0
 		self.start = True
 
+	def text_splitter (self, text):
+		text_splitted = text.split (" ")
+		rec = []
+		s = ""
+		for i in text_splitted:
+			if len(s + i) < 50:
+				s = s + " " +  i
+				continue
+			rec.append (s)
+			s = " " + i
+		rec.append (s)
+		return rec
 
-	def render_text (self,text,answer):
 
-		self.a_max = (len(text)//350)
-		num_end_symb = len(text)%50
-		answer = " "*len(text)+(" "*(100-num_end_symb))+answer 
-		self.super_a_max = len (answer)// 350
-		self.n = self.a*350
+	def render_text (self, text, answer):
+
+		s1 = ''
+		s2 = ''
+		s3 = ''
+		s4 = ''
+		s5 = ''
+		s6 = ''
+		s7 = ''
+
+		all_strokes = self.text_splitter (text)	
+
+		self.n = (self.a * 6)+1
 
 		if self.control.k_n == True:
 			self.control.k_n = False
-			if self.a < self.super_a_max:
+			if self.a < int(((len(all_strokes)) // 6)+1):
 				self.a = self.a+1
 			else:
 				self.a = 0
-			 
-		an1 = answer[self.n+0:self.n+50]
-		an2 = answer[self.n+50:self.n+100]
-		an3 = answer[self.n+100:self.n+150]
-		an4 = answer[self.n+150:self.n+200]
-		an5 = answer[self.n+200:self.n+250]
-		an6 = answer[self.n+250:self.n+300]
-		an7 = answer[self.n+300:self.n+350]	
+		
+		 
+		try:
 
-		information_screen.blit(fonts.font2.render (str(an1), True, (250,250,250)),(2,0))
-		information_screen.blit(fonts.font2.render (str(an2), True, (250,250,250)),(2,22))
-		information_screen.blit(fonts.font2.render (str(an3), True, (250,250,250)),(2,44))
-		information_screen.blit(fonts.font2.render (str(an4), True, (250,250,250)),(2,66))
-		information_screen.blit(fonts.font2.render (str(an5), True, (250,250,250)),(2,88))
-		information_screen.blit(fonts.font2.render (str(an6), True, (250,250,250)),(2,110))
-		information_screen.blit(fonts.font2.render (str(an7), True, (250,250,250)),(2,132))
-	
-		s1 = text[self.n+0:self.n+50]
-		s2 = text[self.n+50:self.n+100]
-		s3 = text[self.n+100:self.n+150]
-		s4 = text[self.n+150:self.n+200]
-		s5 = text[self.n+200:self.n+250]
-		s6 = text[self.n+250:self.n+300]
-		s7 = text[self.n+300:self.n+350]
+			s1 = all_strokes[self.n+0]
+		except IndexError: 
+   			#all_strokes[self.n+1] = None
+   			pass
+		try:
+			s2 = all_strokes[self.n+1]
+		except IndexError: 
+   			#all_strokes[self.n+2] = None
+   			pass
+		try:
+			s3 = all_strokes[self.n+2]
+		except IndexError: 
+   			#all_strokes[self.n+3] = None
+   			pass
+		try:
+			s4 = all_strokes[self.n+3]
+		except IndexError: 
+   			#all_strokes[self.n+4] = None
+   			pass
+		try:
+			s5 = all_strokes[self.n+4]
+		except IndexError: 
+   			#all_strokes[self.n+5] = None
+   			pass
+		try:
+			s6 = all_strokes[self.n+5]
+		except IndexError: 
+   			#all_strokes[self.n+6] = None
+   			pass
+		try:
+			s7 = all_strokes[self.n+6]
+		except IndexError: 
+   			#all_strokes[self.n+7] = None
+   			pass
 
 		information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
 		information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
@@ -67,6 +100,58 @@ class Compose_dialog_tree ():
 		information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
 		information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
 		information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))	
+
+#	def render_text (self,text,answer):
+#
+#
+#
+#
+#		self.a_max = (len(text)//350)
+#		num_end_symb = len(text)%50
+#		answer = " "*len(text)+(" "*(100-num_end_symb))+answer 
+#		self.super_a_max = len (answer)// 350
+#		self.n = self.a*350
+#
+#		if self.control.k_n == True:
+#			self.control.k_n = False
+#			if self.a < self.super_a_max:
+#				self.a = self.a+1
+#			else:
+#				self.a = 0
+#		
+#		all_strokes = text_splitter (text)	 
+#
+#		an1 = answer[self.n+0:self.n+50]
+#		an2 = answer[self.n+50:self.n+100]
+#		an3 = answer[self.n+100:self.n+150]
+#		an4 = answer[self.n+150:self.n+200]
+#		an5 = answer[self.n+200:self.n+250]
+#		an6 = answer[self.n+250:self.n+300]
+#		an7 = answer[self.n+300:self.n+350]	
+#
+#		information_screen.blit(fonts.font2.render (str(an1), True, (250,250,250)),(2,0))
+#		information_screen.blit(fonts.font2.render (str(an2), True, (250,250,250)),(2,22))
+#		information_screen.blit(fonts.font2.render (str(an3), True, (250,250,250)),(2,44))
+#		information_screen.blit(fonts.font2.render (str(an4), True, (250,250,250)),(2,66))
+#		information_screen.blit(fonts.font2.render (str(an5), True, (250,250,250)),(2,88))
+#		information_screen.blit(fonts.font2.render (str(an6), True, (250,250,250)),(2,110))
+#		information_screen.blit(fonts.font2.render (str(an7), True, (250,250,250)),(2,132))
+#	
+#		s1 = text[self.n+0:self.n+50]
+#		s2 = text[self.n+50:self.n+100]
+#		s3 = text[self.n+100:self.n+150]
+#		s4 = text[self.n+150:self.n+200]
+#		s5 = text[self.n+200:self.n+250]
+#		s6 = text[self.n+250:self.n+300]
+#		s7 = text[self.n+300:self.n+350]
+#
+#		information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
+#		information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
+#		information_screen.blit(fonts.font2.render (str(s3), True, (250,250,250)),(2,44))
+#		information_screen.blit(fonts.font2.render (str(s4), True, (250,250,250)),(2,66))
+#		information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
+#		information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
+#		information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))	
 
 
 		#adventure_screen.blit(fonts.font2.render ((str(self.a) + ' self.a'), True, (250,250,250)),(0,35))
@@ -174,59 +259,5 @@ def create_level (level, battle, control):
 	y = 0
 	return  platforms, sprite_group
 
-#def talks (a, n):
-#	z = {1: s1, 2: s2, 3:s3, 4:s4, 5:s5, 6:s6, 7:s7}
-#	z[n] = "a"
-#
-#	information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
-#	information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
-#	information_screen.blit(fonts.font2.render (str(s3), True, (250,250,250)),(2,44))
-#	information_screen.blit(fonts.font2.render (str(s4), True, (250,250,250)),(2,66))
-#	information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
-#	information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
-#	information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))
 
-#def talk (a, n):
-#	'''7 strokes'''
-#	information_screen.blit(fonts.font2.render (str(n), True, (250,250,250)),(2,0+(22*a)))
-
-
-
-#def render_text (text, control, a, s1):
-#	a_max = len(text)//350
-#
-#	n = a*350
-#
-#	if control.k_n == True:
-#		control.k_n = False
-#		s1 = text[n+0:n+50]
-#		if a < a_max+1:
-#			a = a+1
-##		else:
-##			a = 0
-#
-#	#s1 = text[n+0:n+50]
-#	s1 = a
-#	s2 = a_max
-#	#s2 = text[n+50:n+100]
-#	s3 = text[n+100:n+150]
-#	s4 = text[n+150:n+200]
-#	s5 = text[n+200:n+250]
-#	s6 = text[n+250:n+300]
-#	s7 = text[n+300:n+350]
-##	text = s1, s2, s3
-#	information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
-#	information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
-#	information_screen.blit(fonts.font2.render (str(s3), True, (250,250,250)),(2,44))
-#	information_screen.blit(fonts.font2.render (str(s4), True, (250,250,250)),(2,66))
-#	information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
-#	information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
-#	information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))
-
-
-#def text_split (text):
-#	s1 = text[0:50]
-#	s2 = text[50:100]
-#	s3 = text[100:150]
-#	return s1, s2, s3
 
