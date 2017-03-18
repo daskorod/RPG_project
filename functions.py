38fbc5fc9c080d4ab78bf28fc279210ff5084453
daskorod <daskorod@gmail.com>
2017-03-19 02:59:57 +0300

    dialog done

--- functions.py
+++ functions.py
@@ -8,7 +8,6 @@ import text
 def event_go ():
 	c = 0
 
-
 class Compose_dialog_tree ():
 	def __init__ (self, control):
 		self.a_max = 0
@@ -19,8 +18,10 @@ class Compose_dialog_tree ():
 		self.super_a_max = 0
 		self.an = 0
 		self.start = True
+		self.empty = ''
 
 	def text_splitter (self, text):
+
 		text_splitted = text.split (" ")
 		rec = []
 		s = ""
@@ -36,132 +37,38 @@ class Compose_dialog_tree ():
 
 	def render_text (self, text, answer):
 
-		s1 = ''
-		s2 = ''
-		s3 = ''
-		s4 = ''
-		s5 = ''
-		s6 = ''
-		s7 = ''
-
+		s1 = s2 = s3 = s4 = s5 = s6 = s7 = ''
+		ss = [s1,s2,s3,s4,s5,s6,s7]
 		all_strokes = self.text_splitter (text)	
+		all_strokes_ans = self.text_splitter (answer)
+		all_strokes.append (self.empty)
+		all_strokes.extend(all_strokes_ans)
 
-		self.n = (self.a * 6)+1
+		self.n = (self.a * 7)
 
 		if self.control.k_n == True:
 			self.control.k_n = False
-			if self.a < int(((len(all_strokes)) // 6)+1):
+			if self.a < int((len(all_strokes)) // 7):
 				self.a = self.a+1
 			else:
 				self.a = 0
 		
-		 
-		try:
+		counter = 0
+		for i in ss:
 
-			s1 = all_strokes[self.n+0]
-		except IndexError: 
-   			#all_strokes[self.n+1] = None
-   			pass
-		try:
-			s2 = all_strokes[self.n+1]
-		except IndexError: 
-   			#all_strokes[self.n+2] = None
-   			pass
-		try:
-			s3 = all_strokes[self.n+2]
-		except IndexError: 
-   			#all_strokes[self.n+3] = None
-   			pass
-		try:
-			s4 = all_strokes[self.n+3]
-		except IndexError: 
-   			#all_strokes[self.n+4] = None
-   			pass
-		try:
-			s5 = all_strokes[self.n+4]
-		except IndexError: 
-   			#all_strokes[self.n+5] = None
-   			pass
-		try:
-			s6 = all_strokes[self.n+5]
-		except IndexError: 
-   			#all_strokes[self.n+6] = None
-   			pass
-		try:
-			s7 = all_strokes[self.n+6]
-		except IndexError: 
-   			#all_strokes[self.n+7] = None
-   			pass
+			try:
+				ss[counter] = all_strokes [self.n+counter]
+				counter = counter + 1
+			except IndexError:
+				pass
 
-		information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
-		information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
-		information_screen.blit(fonts.font2.render (str(s3), True, (250,250,250)),(2,44))
-		information_screen.blit(fonts.font2.render (str(s4), True, (250,250,250)),(2,66))
-		information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
-		information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
-		information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))	
-
-#	def render_text (self,text,answer):
-#
-#
-#
-#
-#		self.a_max = (len(text)//350)
-#		num_end_symb = len(text)%50
-#		answer = " "*len(text)+(" "*(100-num_end_symb))+answer 
-#		self.super_a_max = len (answer)// 350
-#		self.n = self.a*350
-#
-#		if self.control.k_n == True:
-#			self.control.k_n = False
-#			if self.a < self.super_a_max:
-#				self.a = self.a+1
-#			else:
-#				self.a = 0
-#		
-#		all_strokes = text_splitter (text)	 
-#
-#		an1 = answer[self.n+0:self.n+50]
-#		an2 = answer[self.n+50:self.n+100]
-#		an3 = answer[self.n+100:self.n+150]
-#		an4 = answer[self.n+150:self.n+200]
-#		an5 = answer[self.n+200:self.n+250]
-#		an6 = answer[self.n+250:self.n+300]
-#		an7 = answer[self.n+300:self.n+350]	
-#
-#		information_screen.blit(fonts.font2.render (str(an1), True, (250,250,250)),(2,0))
-#		information_screen.blit(fonts.font2.render (str(an2), True, (250,250,250)),(2,22))
-#		information_screen.blit(fonts.font2.render (str(an3), True, (250,250,250)),(2,44))
-#		information_screen.blit(fonts.font2.render (str(an4), True, (250,250,250)),(2,66))
-#		information_screen.blit(fonts.font2.render (str(an5), True, (250,250,250)),(2,88))
-#		information_screen.blit(fonts.font2.render (str(an6), True, (250,250,250)),(2,110))
-#		information_screen.blit(fonts.font2.render (str(an7), True, (250,250,250)),(2,132))
-#	
-#		s1 = text[self.n+0:self.n+50]
-#		s2 = text[self.n+50:self.n+100]
-#		s3 = text[self.n+100:self.n+150]
-#		s4 = text[self.n+150:self.n+200]
-#		s5 = text[self.n+200:self.n+250]
-#		s6 = text[self.n+250:self.n+300]
-#		s7 = text[self.n+300:self.n+350]
-#
-#		information_screen.blit(fonts.font2.render (str(s1), True, (250,250,250)),(2,0))
-#		information_screen.blit(fonts.font2.render (str(s2), True, (250,250,250)),(2,22))
-#		information_screen.blit(fonts.font2.render (str(s3), True, (250,250,250)),(2,44))
-#		information_screen.blit(fonts.font2.render (str(s4), True, (250,250,250)),(2,66))
-#		information_screen.blit(fonts.font2.render (str(s5), True, (250,250,250)),(2,88))
-#		information_screen.blit(fonts.font2.render (str(s6), True, (250,250,250)),(2,110))
-#		information_screen.blit(fonts.font2.render (str(s7), True, (250,250,250)),(2,132))	
-
-
-		#adventure_screen.blit(fonts.font2.render ((str(self.a) + ' self.a'), True, (250,250,250)),(0,35))
-		#adventure_screen.blit(fonts.font2.render ((str(self.an) + " self.an"), True, (250,250,250)),(0,55))
-		#adventure_screen.blit(fonts.font2.render ((str(self.super_a_max) + " super a"), True, (250,250,250)),(0,75))
-		#adventure_screen.blit(fonts.font2.render ((str(self.a_max) + " self.a_max"), True, (250,250,250)),(0,95))
-
-
-
-
+		information_screen.blit(fonts.font2.render (str(ss[0]), True, (250,250,250)),(2,0))
+		information_screen.blit(fonts.font2.render (str(ss[1]), True, (250,250,250)),(2,22))
+		information_screen.blit(fonts.font2.render (str(ss[2]), True, (250,250,250)),(2,44))
+		information_screen.blit(fonts.font2.render (str(ss[3]), True, (250,250,250)),(2,66))
+		information_screen.blit(fonts.font2.render (str(ss[4]), True, (250,250,250)),(2,88))
+		information_screen.blit(fonts.font2.render (str(ss[5]), True, (250,250,250)),(2,110))
+		information_screen.blit(fonts.font2.render (str(ss[6]), True, (250,250,250)),(2,132))	
 
 class Battle ():
 	def __init__ (self, control):
